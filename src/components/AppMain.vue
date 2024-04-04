f393428f2d3842581dc854b73dc46f22177edbdb
MassimoRomano <romano93m@gmail.com>
2024-04-04 19:45:30 +0200

    fix wrap and add img

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,20 +4,28 @@ export default {
     props: {
         movies: Array,
         errorMessage: String
+    },
+    methods: {
+        getImageUrl(posterPath) {
+            return posterPath ? `https://image.tmdb.org/t/p/w500/${posterPath}` : 'placeholder.jpg';
+        }
     }
 };
 </script>
 
 <template>
-   <main>
+    <main>
         <div class="movie-list">
             <div v-if="errorMessage" class="error-message">{{ errorMessage }}</div>
             <div v-else>
-                <div v-for="movie in movies" :key="movie.id" class="movie">
-                    <h2>{{ movie.title }}</h2>
-                    <p>Titolo Originale: {{ movie.original_title }}</p>
-                    <p>Lingua: {{ movie.original_language }}</p>
-                    <p>Voto: {{ movie.vote_average }}</p>
+                <div class="d-wrap">
+                    <div v-for="movie in movies" :key="movie.id" class="movie">
+                        <img :src="getImageUrl(movie.poster_path)" :alt="movie.title">
+                        <h2>{{ movie.title }}</h2>
+                        <p>Titolo Originale: {{ movie.original_title }}</p>
+                        <p>Lingua: {{ movie.original_language }}</p>
+                        <p>Voto: {{ movie.vote_average }}</p>
+                    </div>
                 </div>
             </div>
         </div>
