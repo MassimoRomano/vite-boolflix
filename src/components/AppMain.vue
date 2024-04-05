a596a67a841aec87b764767bdc09e4c4a84fd6c3
MassimoRomano <romano93m@gmail.com>
2024-04-05 17:44:54 +0200

    add fix css

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,9 +10,9 @@ export default {
             return posterPath ? `https://image.tmdb.org/t/p/w500/${posterPath}` : 'placeholder.jpg';
         },
         getStarRating(vote) {
-            const roundedVote = Math.ceil(vote / 2); 
+            const roundedVote = Math.ceil(vote / 2);
             const stars = [];
-            
+
             for (let i = 0; i < 5; i++) {
                 if (i < roundedVote) {
                     stars.push('<i class="fa-solid fa-star"></i>'); // Stella piena
@@ -22,7 +22,7 @@ export default {
                     stars.push('<i class="fa-regular fa-star"></i>'); // Stella vuota
                 }
             }
-            
+
             return stars;
         }
     }
@@ -37,12 +37,16 @@ export default {
                 <div class="d-wrap">
                     <div v-for="(item, index) in media" :key="index" class="movie">
                         <img :src="getImageUrl(item.poster_path)" :alt="item.title">
-                        <img style="width: 30px;" :src="item.flag" :alt="item.original_language" class="flag">
-                        <h2>{{ item.title }}</h2>
-                        <p>Titolo Originale: {{ item.original_title || item.original_name }}</p>
-                        <p>Lingua: {{ item.original_language }}</p>
-                        <div class="star-rating">
-                            <span v-html="getStarRating(item.vote_average).join('')"></span>
+                        <div class="movie-info">
+                            <div class="info">
+                                <img style="width: 30px;" :src="item.flag" :alt="item.original_language" class="flag">
+                                <h2>{{ item.title }}</h2>
+                                <p>Titolo Originale: {{ item.original_title || item.original_name }}</p>
+                                <p>Lingua: {{ item.original_language }}</p>
+                                <div class="star-rating">
+                                    <span v-html="getStarRating(item.vote_average).join('')"></span>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
